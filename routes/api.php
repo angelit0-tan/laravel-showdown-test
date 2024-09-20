9ae8848dad90318c1ed40b6936f516d3d01db382
shiftdelete <angelito.tan23@gmail.com>
2024-09-20 14:43:54 +0800

    feat: batch request

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BatchController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::post('/batch', [BatchController::class, 'index']);
